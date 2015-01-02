014265ecb4ee06c9a0c107c9d88868db45e76f40
Aniruddha <legacy@localhost.localdomain>
2015-01-02 18:45:37 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
-Key and CSR Generator
+### Key and CSR Generator
 
-Incase their are multiple domains for each of which a key and csr is required, user cannot go on executing same commands everytime. Executing the script will create key and csr files for domains which are specified in different file named 'domains'. Password for csr generation is specified in separate file 'pass' to avoid being viewed under utilities like 'ps'.
+Incase their are multiple domains for each of which a key and csr is required, user cannot go on executing same commands everytime. Executing the script will create key and csr files for domains which are specified in different file named **domains**. Password for csr generation is specified in separate file **pass** to avoid being viewed under utilities like *ps*.
 
 The Script will be loaded with default Subject values for csr genetration. User is asked to change subject values and generate csr.
 Following are default Subject Values:
-country=.
-state=.
-locality=.
-orgname=.
-orgunit=.
-companyname=.
-email=.
+* country=.
+* state=.
+* locality=.
+* orgname=.
+* orgunit=.
+* companyname=.
+* email=.
 
+User is provided with option to input subject values. User is required to follow constraints applied over input of subject values.
 
-Running the script
+##### Running the script
 
 No root privileges in order to run the script. No excutable permissions given, hence use bash inorder to run the script. Download the zip and extract it. Go to the extracted directoy and run the command
 
--- create_key+csr.sh
+++ create_key+csr.sh
@@ -1,4 +1,4 @@
-:' Script is to create key and csr files for the domain which are maintiained in separate file.'
+: 'Script is to create key and csr files for the domain which are maintiained in separate file.'
 
 #!/bin/bash
 
@@ -10,17 +10,17 @@ country=.
 state=.
 locality=.
 orgname=.
-orgunit='Domain Control Validated'
-companyname=AllNaturalThin.com
+orgunit=.
+companyname=.
 email=.
 
-printf "Subject values to be passed while creating csr
-${red}country=.
+printf "Subject values to be passed while creating csr\n
+${blue}country=.
 state=.
 locality=.
 orgname=.
-orgunit='Domain Control Validated'
-companyname=AllNaturalThin.com
+orgunit=.
+companyname=.
 email=.${nc}
 
 Select from options below
@@ -30,34 +30,48 @@ read bool
 
 if [ $bool -eq 1 ]
 then
-	printf "Enter new subject values:\nCountry="
+	printf "Enter new subject values:\nCountry (2 letter code) [US]="
 	read country
-	printf "State="
+
+	while [ `echo $country | wc -L` -ne 2 ]		                          #Check if country name is equal to 2 characters
+	do
+		printf "${red}Country Name should be 2 Characters e.g., US.\n${blue}Enter New one=${nc}"
+		read country
+	done
+
+	printf "State [Enter . for empty value]="
 	read state
-	printf "Locality="
+	printf "Locality [Enter . for empty value]="
 	read locality
-	printf "Organisation Name="
+	printf "Organisation Name [Enter . for empty value]="
 	read orgname
-	printf "Organisation Unit="
+	printf "Organisation Unit [Enter . for empty value]="
 	read orgunit
-	printf "Company Name="
+	printf "Company Name (max 64 letter) [Enter . for empty value]="
 	read companyname
-	printf "Email-Id="
+	echo $companyname
+	
+	if [ "$companyname"=="" ]						#check if company name is not null
+	then
+		companyname=.
+	fi
+
+	printf "Email-Id (max 64 letter) [Enter . for empty value]="
 	read email
-	echo "Applying values"
+
+	printf "\n\n${blue}Applying new subject values\n\n${nc}"
 
 	for line in `cat ./domains`
 	do
 		echo "--------------------------------------------------------$line-----------------------------------------------------------"
-		openssl genrsa -des3 -out $line.key -passout pass:$passwd 2048
-		printf "\n${blue}KEY for $line is generated successfully.${nc}\n"
+		openssl genrsa -des3 -out $line.key -passout file:./pass 2048			#for generating key. Password is taken from 'pass' file.
 		
-		openssl req -nodes -new -key $line.key -out $line.csr -passin pass:$passwd -subj "/C=$country /ST=$state /L=$locality /O=$orgname /OU=$orgunit /CN=$companyname"
+		openssl req -nodes -new -key $line.key -out $line.csr -passin file:./pass -subj "/C=$country/ST=$state/L=$locality/O=$orgname/OU=$orgunit/CN=$companyname"							#for generating csr with user input subject values. Password is taken from 'pass' file.
 		if [ `echo $?` -eq 0 ]
 		then
-			printf "${blue}CSR for $line is generated successfully.${nc}\n"
+			printf "${blue}KEY and CSR for $line is generated successfully.${nc}\n"
 		else
-			printf "\n${red}Error while creating CSR.${nc}\n\n"
+			printf "\n${red}Key generated successfully. Error while creating CSR.${nc}\n\n"
 		fi
 	done
 
@@ -66,17 +80,16 @@ then
 	for line in `cat ./domains`
 	do
 		echo "--------------------------------------------------------$line-----------------------------------------------------------"
-		printf "\n${blue}KEY for $line is generated successfully.${nc}\n"
-		openssl genrsa -des3 -out $line.key -passout file:./pass 2048
+		openssl genrsa -des3 -out $line.key -passout file:./pass 2048			#for generating key. Password is taken from 'pass' file.
+
+		openssl req -nodes -new -key $line.key -out $line.csr -passin file:./pass -subj "/C=$country /ST=$state /L=$locality /O=$orgname /OU=$orgunit /CN=$companyname"				#for generating csr with default subject values. Password is taken from 'pass' file.
 
 		if [ `echo $?` -eq 0 ]
 		then
-			printf "${blue}CSR for $line is generated successfully.${nc}\n\n"
+			printf "${blue}KEY and CSR for $line is generated successfully.${nc}\n\n"
 		else
-			printf "${red}Error while creating CSR.${nc}\n"
+			printf "${red}Key generated successfully. Error while creating CSR.${nc}\n"
 		fi
-		
-		openssl req -nodes -new -key $line.key -out $line.csr -passin file:./pass -subj "/C=$country /ST=$state /L=$locality /O=$orgname /OU=$orgunit /CN=$companyname"
 	done
 
 else
